2c23765bb92b75dd4c7d5a69a4b31c01e0bb0b8c
Athena <silva.ad@obmontessori.edu.ph>
2026-09-07 14:14:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#Silva, Datiles
+studname = "River Datiles" #string
+studage = 15 #integer
+studheight_1 = 161.1 #float
+countries_to_visit =  set(['Poland','Japan', 'United Kingdom'])
+student_type = False #boolean
+student_info = {
+    "color": "Blue",
+    "car_brand": "Porche",
+    "shoe_size": 39,
+    "best_friend": "Milli"}  #dictionary
+yummy_fruit_set = {"Mango", "Melon", "Watermelon", "Apple", "Durian"}  #set
+weekdays = (
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+)  #tuple
+
